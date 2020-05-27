198eb5881babc48dc78489e14e7adbb037703f25
KevinChanD <50549173+KevinChanD@users.noreply.github.com>
2020-05-27 11:43:58 +0800

    Update WordCount.cpp

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -3,10 +3,36 @@
 
 #include <iostream>
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
-	cout << "2";
+	int i = 0;
+	int n = 0;//记录要求记录的个数
+	int t = 0;//用于数据读取
+	FILE *fp;
+	fp = fopen_s(argv[2], "r");//以只读方式打开文件
+	char buf[BUFSIZ];//建立一个缓冲区，放置文件内容
+	while (!feof(fp))
+	{
+		if (fscanf_s(fp, "%c", &buf[t]) != 1)
+			break;
+		t++;
+		fgetc(fp);//过滤度掉分隔符知
+	}
+	fclose(fp);
+	if (!strcmp("-c", argv[1])) {
+		printf("字符数量是%d\n", (int)strlen(buf));
+	}//处理字符数
+	else {
+		while (i != strlen(buf))
+		{
+			if (buf[i] == ',' || buf[i] == ' ')
+				n++;
+			i++;
+		}
+		if (buf[i - 1] != ',') n++;
+		printf("单词数量是%d\n", n);
+	}//处理单词数
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
